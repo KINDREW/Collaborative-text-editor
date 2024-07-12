997d04c370b089bf4361ac81ea64bf8ef42f06cd
kindrew <kindrew99@gmail.com>
2024-07-12 16:32:18 +0000

    added shareJs

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const sharejs = require("share");
+require("redis");
 
 const port = process.env.PORT || 8000;
 app.listen(port);
+
+// options for sharejs
+var options = {
+  db: { type: "redis" },
+};
+
+// attach the express server to sharejs
+sharejs.server.attach(app, options);
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
@@ -12,5 +22,9 @@ app.use(express.static(__dirname + "/public"));
 
 // routes for app
 app.get("/", function (req, res) {
+  res.render("pad");
+});
+
+app.get("/(:id)", function (req, res) {
   res.render("pad");
 });
